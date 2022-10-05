951858bd4bdee8d0dc043fe7d21e5eb3b1d26f74
Peter Nguyen <nguyen.peter.viet@gmail.com>
2022-10-05 16:31:34 -0700

    multi-map done

--- problems/03-multi-map.js
+++ problems/03-multi-map.js
@@ -22,8 +22,27 @@ console.log(result3); // hi!!!!!
 *******************************************************************************/
 
 function multiMap(val, n, cb) {
-  // Your code here
+  let prod = val;
+  for (let i = 0; i < n; i++) {
+    prod = cb(prod)
+  }
+  return prod;
 }
+
+let result1 = multiMap(7, 2, function(n) {
+  return n * 10;
+});
+console.log(result1); // 700
+
+let result2 = multiMap(7, 3, function(n) {
+    return n * 10;
+});
+console.log(result2); // 7000
+
+let result3 = multiMap("hi", 5, function(s) {
+  return s + "!";
+});
+console.log(result3); // hi!!!!!
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
